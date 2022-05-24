51f626fb4a44d1f9a08b5236c98c91829edfe769
Zainab Rizwan <76821973+Zainab-Rizwan@users.noreply.github.com>
2022-05-24 10:32:06 +0500

    Delete Task 1 directory

--- Task 1/clientside.py
+++
@@ -1,9 +0,0 @@
-from email import message
-from socket import *
-serverName = 'localhost'
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-message=input("Please enter filename to be displayed: ")
-clientSocket.send(message.encode())
-clientSocket.close()
